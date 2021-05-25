3208fc8fdad805eff75ed0efafeb56421276bc49
IsmMah <ismat.mahmood@softwire.com>
2021-05-25 12:07:57 +0100

    commit 2

--- DynamitBot.js
+++ DynamitBot.js
@@ -1,7 +1,55 @@
 class Bot {
+
     makeMove(gamestate) {
-        return 'R';
+
+    let rounds = new Array; 
+    rounds = gamestate.rounds;
+    let p1Count = 0;
+    let p2Count = 0;
+
+    for (let i = 0; i< rounds.length;i++){
+        let p1Check = rounds[i].p1;
+        if (p1Check === 'D'){
+            p1Count++;
+        }
+        let p2Check = rounds[i].p2;
+        if (p2Check === 'D'){
+            p2Count++;
+        }  
     }
+
+    if(p1Count<100){ // we have dynamight so we should use it
+        // play dynamight 
+        return 'D';
+    }else if(p2Count<100){ // P2 still has dynamight
+        
+        // play with waterbomb
+        let temp = Math.random();
+        if(temp<0.25){
+            return 'R';
+        }else if (temp<0.5){
+            return 'P';
+        }else if (temp<0.75){
+            return 'S';
+        }else {
+            return 'W';
+        }
+
+    }else { // p2 has no dynamight left
+        // play no waterbomb
+        let temp = Math.random();
+        if(temp<0.33){
+            return 'R';
+        }else if (temp<0.66){
+            return 'P';
+        }else {
+            return 'S';
+
+    }
+
 }
 
+
 module.exports = new Bot();
+
+    
---
+++ debug.js
@@ -0,0 +1,15 @@
+let random = Math.floor(Math.random()*100);
+if (random<25){
+    console.log('R');
+}
+else if (random <50){
+    console.log('P');
+}
+else if (random < 75){
+    console.log('S');
+}
+else {
+    console.log('W')
+}
+
+console.log(random);
